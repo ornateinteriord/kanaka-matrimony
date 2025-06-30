e53833390ae588cdbdb721002cd67c0993f490ae
sid-1974 <sidgithub1974@gmail.com>
2025-06-30 14:46:28 +0530

    import file

--- src/components/howWorks/Connect.jsx
+++ src/components/howWorks/Connect.jsx
@@ -2,7 +2,7 @@
 import { Container, Typography, Grid, Box, Paper, Badge, Card, CardContent } from '@mui/material';
 import { FaUserEdit, FaUsers, FaComments, FaUserFriends, FaCalendarWeek, FaCalendarAlt, FaUserPlus } from 'react-icons/fa';
 import { useGetDashboardStats } from '../api/Auth';
-import '../variables/Variables.scss'
+
 
 const Connect = () => {
   const { data: dashboardstats } = useGetDashboardStats();
